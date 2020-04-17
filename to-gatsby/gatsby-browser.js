2a07859893bf63e80d76da6f7d696895b4ac85f2
Matt Hamrick <matthewjhamrick@gmail.com>
2020-04-17 22:36:54 +0000

    did a bit more layout setup.

--- to-gatsby/gatsby-browser.js
+++ to-gatsby/gatsby-browser.js
@@ -5,3 +5,27 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+const { ThemeProvider } = require("@material-ui/core")
+const { createMuiTheme } = require("@material-ui/core/styles")
+const orange = require("@material-ui/core/colors/orange")
+
+console.log(orange)
+
+const theme = createMuiTheme({
+  palette: {
+    primary: orange.default,
+  },
+  typography: palette => ({
+    // What does this mean???
+    fontFamily: "inherit",
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 300,
+    },
+  }),
+})
+
+exports.wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={theme}>{element}</ThemeProvider>
+}
--- to-gatsby/src/components/layout.tsx
+++ to-gatsby/src/components/layout.tsx
@@ -6,24 +6,38 @@
  */
 
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import { makeStyles } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
 import { Link } from "gatsby"
+import { Home } from "@material-ui/icons"
+import classnames from "classnames"
 
 import "./layout.css"
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100%",
+    display: "flex",
+  },
   nav: {
+    minWidth: "260px",
+    borderRight: `1px solid ${theme.palette.grey[200]}`,
+    color: theme.palette.getContrastText(theme.palette.grey[800]),
+    backgroundColor: theme.palette.grey[800],
     "& ol": {
-      listStyle: "none",
+      //  TODO - I should probably do this via css-reset.
       margin: 0,
       padding: 0,
+      paddingTop: theme.spacing(1),
+      listStyle: "none",
+      width: "100%",
       "& li": {
+        width: "100%",
+        display: "flex",
         "& a": {
-          padding: theme.spacing(1),
+          color: "unset",
+          width: "100%",
           textDecoration: "none",
-          color: theme.palette.text.primary,
           "&:hover": {
             backgroundColor: theme.palette.grey[100],
             color: theme.palette.getContrastText(theme.palette.grey[100]),
@@ -32,42 +46,70 @@ const useStyles = makeStyles(theme => ({
       },
     },
   },
-  main: {
-    margin: "auto",
+  innerNav: {
+    padding: theme.spacing(1),
+    paddingLeft: theme.spacing(4),
   },
+  home: {
+    display: "flex",
+    alignItems: "center",
+  },
+  homeIcon: {
+    padding: theme.spacing(1),
+  },
+  main: {
+    flexGrow: 1,
+    padding: theme.spacing(4),
+  },
+  content: {},
 }))
 
 interface LayoutProps {
-  title?: string
+  title: string
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, title }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
   const classes = useStyles()
 
   return (
-    <>
+    <div className={classes.root}>
       <nav className={classes.nav}>
-        <Typography variant="h5">Demos & Tools</Typography>
         <ol>
           <li>
-            <Link to="/autotrack/">Autotrack</Link>
+            <Link to="/">
+              <Typography
+                className={classnames(classes.innerNav, classes.home)}
+                variant="h5"
+              >
+                <Home className={classes.homeIcon} /> Home
+              </Typography>
+            </Link>
           </li>
           <li>
-            <Link to="/account-explorer/">Account Explorer</Link>
+            <Typography className={classes.innerNav} variant="h6">
+              Demos & Tools
+            </Typography>
+          </li>
+          <li>
+            <Link className={classes.innerNav} to="/autotrack/">
+              Autotrack
+            </Link>
+          </li>
+          <li>
+            <Link className={classes.innerNav} to="/account-explorer/">
+              Account Explorer
+            </Link>
           </li>
         </ol>
       </nav>
-      <main className={classes.main}>{children}</main>
-    </>
+      <main className={classes.main}>
+        <header>
+          TODO - add in header picture
+          <Typography variant="h1">{title}</Typography>
+        </header>
+        {children}
+      </main>
+    </div>
   )
 }
 
