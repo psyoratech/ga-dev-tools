b3cca0e6c75904efcb97c7d42b45701c2070f66c
Matt Hamrick <matthewjhamrick@gmail.com>
2020-04-20 20:37:41 +0000

    Updated urls to values from the constant & ul's under p's.

---
+++ to-gatsby/src/constants.ts
@@ -0,0 +1,9 @@
+// All static links on the site should be put into this constant. This makes it
+// easier to update all instances of a link on the site.
+export const Url = {
+  gaDevToolsGitHub: "https://github.com/googleanalytics/ga-dev-tools",
+  gaDevToolsGitHubNewIssue:
+    "https://github.com/googleanalytics/ga-dev-tools/issues/new",
+  gaDevsite: "http://developers.google.com/analytics",
+  gaDevsiteHelp: "http://developers.google.com/analytics/help/",
+}
--- to-gatsby/src/pages/index.tsx
+++ to-gatsby/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
+import { Url } from "../constants"
 
 const useStyles = makeStyles({ partnersImage: { maxWidth: "600px" } })
 
@@ -41,59 +42,56 @@ const IndexPage = () => (
     </Typography>
     <Typography variant="body1">
       The code for this entire site is open source and{" "}
-      <a href="#TODO">available on GitHub</a>. We encourage you to take a look
-      if you'd like to see how anything is done.
+      <a href={Url.gaDevToolsGitHub}>available on GitHub</a>. We encourage you
+      to take a look if you'd like to see how anything is done.
     </Typography>
     <Typography variant="h3">Demos</Typography>
     <Typography variant="body1">
       Live demos to help you learn about Google Analytics features.
-      <ul>
-        <li>Working demos with actual data sent to Google Analytics.</li>
-        <li> Use these demos as a reference for your own implementation.</li>
-        <li>Interactive elements that provide implementation code snippets.</li>
-      </ul>
+    </Typography>
+    <Typography variant="body1" component="ul">
+      <li>Working demos with actual data sent to Google Analytics.</li>
+      <li> Use these demos as a reference for your own implementation.</li>
+      <li>Interactive elements that provide implementation code snippets.</li>
     </Typography>
     <Typography variant="h3">Tools</Typography>
     <Typography variant="body1">
       Tools to showcase how Google Analytics can be extendend with custom
       solutions.
-      <ul>
-        <li>Tools are targeted at real-world use cases and solutions.</li>
-        <li>
-          Access advanced Google Analytics features without writing any code.
-        </li>
-        <li>
-          All publicly available technologies are used. Nothing proprietary.
-        </li>
-      </ul>
+    </Typography>
+    <Typography variant="body1" component="ul">
+      <li>Tools are targeted at real-world use cases and solutions.</li>
+      <li>
+        Access advanced Google Analytics features without writing any code.
+      </li>
+      <li>
+        All publicly available technologies are used. Nothing proprietary.
+      </li>
     </Typography>
     <Typography variant="h2">Help &amp; Feedback</Typography>
     <Typography variant="h3">For this site</Typography>
-    <Typography variant="body1">
-      <ul>
-        <li>
-          You may report bugs by <a href="#TODO">submitting an issue</a> on
-          GitHub.
-        </li>
-        <li>
-          You may also use GitHub to{" "}
-          <a href="#TODO">request a new demo or tool</a>.
-        </li>
-      </ul>
+    <Typography variant="body1" component="ul">
+      <li>
+        You may report bugs by{" "}
+        <a href={Url.gaDevToolsGitHubNewIssue}>submitting an issue</a> on
+        GitHub.
+      </li>
+      <li>
+        You may also use GitHub to{" "}
+        <a href={Url.gaDevToolsGitHubNewIssue}>request a new demo or tool</a>.
+      </li>
     </Typography>
     <Typography variant="h3">For the Google Analytics platform</Typography>
-    <Typography variant="body1">
-      <ul>
-        <li>
-          Documentation for all Google Analytics API, libraries and SDKs can be
-          found on <a href="#TODO">Google Analytics Developers</a>.
-        </li>
-        <li>
-          If you have questions, please refer to the{" "}
-          <a href="#TODO">getting help</a> section of the developers site to
-          find the best place to get your questions answered.
-        </li>
-      </ul>
+    <Typography component="ul" variant="body1">
+      <li>
+        Documentation for all Google Analytics API, libraries and SDKs can be
+        found on <a href={Url.gaDevsite}>Google Analytics Developers</a>.
+      </li>
+      <li>
+        If you have questions, please refer to the{" "}
+        <a href={Url.gaDevsiteHelp}>getting help</a> section of the developers
+        site to find the best place to get your questions answered.
+      </li>
     </Typography>
   </Layout>
 )
